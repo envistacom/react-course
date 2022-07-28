a7d844a753f0fab33c29ac7dbc9ab67080a099ff
EnvistacomWhiteHat <pechjacobenvistacom@gmail.com>
2022-07-28 16:34:48 -0600

    Creating Components

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -5,8 +5,19 @@ function App() {
   return (
     <div className="App">
       <p>The test</p>
+      <Info />
+      <Info />
     </div>
   );
+}
+
+function Info() {
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>Text and stuff</p>
+    </div>
+  )
 }
 
 export default App;
