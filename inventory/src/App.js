870e3f119d0d998a3e6441d4976986e92db4e073
EnvistacomWhiteHat <pechjacobenvistacom@gmail.com>
2022-07-29 11:07:19 -0600

    npm install to npm ci and test coverage

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="Silly logo"/>
-      <p>This is from app.js</p>
+      <p>learn react</p>
       <Info />
       <Info />
     </div>
