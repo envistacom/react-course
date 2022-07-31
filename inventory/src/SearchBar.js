c25ea85db1be320b49cd7070a29e0c75de985231
EnvistacomWhiteHat <pechjacobenvistacom@gmail.com>
2022-07-30 20:04:38 -0600

    Test

--- inventory/src/SearchBar.js
+++ inventory/src/SearchBar.js
@@ -6,18 +6,25 @@ function SearchBar(props) {
     const [type, setType] = useState("");
     const [brand, setBrand] = useState("");
 
+    const searchButtonPressed = () => {
+        console.log(name);
+        console.log(price);
+        console.log(type);
+        console.log(brand);
+        }
+
     return (
         <div>
             <h2>Search for an Item</h2>
             <form>
-                <label for="name-field">Name:</label>
+                <label htmlFor="name-field">Name:</label>
                 <input
                     id="name-field"
                     type="text"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                 />
-                <label for="price-field">Price:</label>
+                <label htmlFor="price-field">Price:</label>
                 <input
                     id="price-field"
                     type="text"
@@ -31,14 +38,14 @@ function SearchBar(props) {
                     value={type}
                     onChange={(e) => setType(e.target.value)}                
                 />
-                <label for="brand-field">Brand:</label>
+                <label htmlFor="brand-field">Brand:</label>
                 <input
                     id="brand-field"
                     type="text"
                     value={brand}
                     onChange={(e) => setBrand(e.target.value)}
                 />
-                <button type="button">Search</button>
+                <button type="button" onClick={searchButtonPressed }>Search</button>
             </form>
         </div>
     )
